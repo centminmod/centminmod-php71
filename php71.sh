b95b8dfe02982a04dd94beee02ed2dfaf53c78be
George Liu <eva2000@centminmod.com>
2017-06-07 00:46:50 +1000

    initial commit

---
+++ php71.sh
@@ -0,0 +1,249 @@
+#/bin/bash
+############################################
+# centminmod.com multiple PHP-FPM version
+# installer for PHP 7.1 branch installed
+# side by side concurrently with default
+# centmin mod installed PHP version using
+# using Remi YUM repo's SCL php71 version
+# written by George Liu centminmod.com
+############################################
+DT=$(date +"%d%m%y-%H%M%S")
+CENTMINLOGDIR='/root/centminlogs'
+repoopt='--disableplugin=priorities --disableexcludes=main,remi --enablerepo=remi'
+packages='php71 php71-php-fpm php71-php-devel php71-php-mysqlnd php71-php-bcmath php71-php-enchant php71-php-gd php71-php-pecl-geoip php71-php-gmp php71-php-pecl-igbinary php71-php-pecl-igbinary-devel php71-php-pecl-imagick php71-php-pecl-imagick-devel php71-php-imap php71-php-intl php71-php-pecl-json-post php71-php-ldap php71-php-pecl-mailparse php71-php-mbstring php71-php-mcrypt php71-php-pecl-memcache php71-php-pecl-memcached php71-php-pecl-mysql php71-php-pdo-dblib php71-php-pspell php71-php-pecl-redis php71-php-snmp php71-php-soap php71-php-tidy php71-php-xml php71-php-xmlrpc php71-php-pecl-zip php71-php-opcache'
+
+############################################
+# set locale temporarily to english
+# due to some non-english locale issues
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+shopt -s expand_aliases
+for g in "" e f; do
+    alias ${g}grep="LC_ALL=C ${g}grep"  # speed-up grep, egrep, fgrep
+done
+
+CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
+
+if [ ! -d "$CENTMINLOGDIR" ]; then
+  mkdir -p "$CENTMINLOGDIR"
+fi
+
+if [ "$CENTOSVER" == 'release' ]; then
+    CENTOSVER=$(awk '{ print $4 }' /etc/redhat-release | cut -d . -f1,2)
+    if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+    fi
+fi
+
+if [[ "$(cat /etc/redhat-release | awk '{ print $3 }' | cut -d . -f1)" = '6' ]]; then
+    CENTOS_SIX='6'
+fi
+
+if [ "$CENTOSVER" == 'Enterprise' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
+    OLS='y'
+fi
+
+if [[ -f /etc/system-release && "$(awk '{print $1,$2,$3}' /etc/system-release)" = 'Amazon Linux AMI' ]]; then
+    CENTOS_SIX='6'
+fi
+
+if [[ "$CENTOS_SEVEN" -ne '7' ]]; then
+  echo
+  echo "CentOS 7 Only"
+  echo "aborting..."
+  echo
+  exit
+fi
+
+
+phpsededit() {
+    TOTALMEM=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+    PHPINICUSTOM='zzz_customphp.ini'
+    CONFIGSCANDIR='/etc/opt/remi/php71/php.d'
+    CUSTOMPHPINIFILE="${CONFIGSCANDIR}/${PHPINICUSTOM}"
+
+    if [[ ! -f "${CUSTOMPHPINIFILE}" ]]; then
+        touch ${CUSTOMPHPINIFILE}
+    else
+        \cp -a ${CUSTOMPHPINIFILE} ${CUSTOMPHPINIFILE}-bak_$DT
+        rm -rf $CUSTOMPHPINIFILE
+        rm -rf ${CONFIGSCANDIR}/custom_php.ini
+        echo "" > ${CUSTOMPHPINIFILE}
+    fi
+
+    if [[ "$(date +"%Z")" = 'EST' ]]; then
+        echo "date.timezone = Australia/Brisbane" >> ${CUSTOMPHPINIFILE}
+    else
+        echo "date.timezone = UTC" >> ${CUSTOMPHPINIFILE}
+    fi
+
+    # dynamic PHP memory_limit calculation
+    if [[ "$TOTALMEM" -le '262144' ]]; then
+        ZOLIMIT='32'
+        PHP_MEMORYLIMIT='48M'
+        PHP_UPLOADLIMIT='48M'
+        PHP_REALPATHLIMIT='256k'
+        PHP_REALPATHTTL='14400'
+    elif [[ "$TOTALMEM" -gt '262144' && "$TOTALMEM" -le '393216' ]]; then
+        ZOLIMIT='80'
+        PHP_MEMORYLIMIT='96M'
+        PHP_UPLOADLIMIT='96M'
+        PHP_REALPATHLIMIT='320k'
+        PHP_REALPATHTTL='21600'
+    elif [[ "$TOTALMEM" -gt '393216' && "$TOTALMEM" -le '524288' ]]; then
+        ZOLIMIT='112'
+        PHP_MEMORYLIMIT='128M'
+        PHP_UPLOADLIMIT='128M'
+        PHP_REALPATHLIMIT='384k'
+        PHP_REALPATHTTL='28800'
+    elif [[ "$TOTALMEM" -gt '524288' && "$TOTALMEM" -le '1049576' ]]; then
+        ZOLIMIT='144'
+        PHP_MEMORYLIMIT='160M'
+        PHP_UPLOADLIMIT='160M'
+        PHP_REALPATHLIMIT='384k'
+        PHP_REALPATHTTL='28800'
+    elif [[ "$TOTALMEM" -gt '1049576' && "$TOTALMEM" -le '2097152' ]]; then
+        ZOLIMIT='240'
+        PHP_MEMORYLIMIT='256M'
+        PHP_UPLOADLIMIT='256M'
+        PHP_REALPATHLIMIT='384k'
+        PHP_REALPATHTTL='28800'
+    elif [[ "$TOTALMEM" -gt '2097152' && "$TOTALMEM" -le '3145728' ]]; then
+        ZOLIMIT='304'
+        PHP_MEMORYLIMIT='320M'
+        PHP_UPLOADLIMIT='320M'
+        PHP_REALPATHLIMIT='512k'
+        PHP_REALPATHTTL='43200'
+    elif [[ "$TOTALMEM" -gt '3145728' && "$TOTALMEM" -le '4194304' ]]; then
+        ZOLIMIT='496'
+        PHP_MEMORYLIMIT='512M'
+        PHP_UPLOADLIMIT='512M'
+        PHP_REALPATHLIMIT='512k'
+        PHP_REALPATHTTL='43200'
+    elif [[ "$TOTALMEM" -gt '4194304' ]]; then
+        ZOLIMIT='784'
+        PHP_MEMORYLIMIT='800M'
+        PHP_UPLOADLIMIT='800M'
+        PHP_REALPATHLIMIT='640k'
+        PHP_REALPATHTTL='86400'
+    fi
+
+    echo "max_execution_time = 60" >> ${CUSTOMPHPINIFILE}
+    echo "short_open_tag = On" >> ${CUSTOMPHPINIFILE}
+    echo "realpath_cache_size = $PHP_REALPATHLIMIT" >> ${CUSTOMPHPINIFILE}
+    echo "realpath_cache_ttl = $PHP_REALPATHTTL" >> ${CUSTOMPHPINIFILE}
+    echo "upload_max_filesize = $PHP_UPLOADLIMIT" >> ${CUSTOMPHPINIFILE}
+    echo "memory_limit = $PHP_MEMORYLIMIT" >> ${CUSTOMPHPINIFILE}
+    echo "post_max_size = $PHP_UPLOADLIMIT" >> ${CUSTOMPHPINIFILE}
+    echo "expose_php = Off" >> ${CUSTOMPHPINIFILE}
+    echo "mail.add_x_header = Off" >> ${CUSTOMPHPINIFILE}
+    echo "max_input_nesting_level = 128" >> ${CUSTOMPHPINIFILE}
+    echo "max_input_vars = 10000" >> ${CUSTOMPHPINIFILE}
+    echo "mysqlnd.net_cmd_buffer_size = 16384" >> ${CUSTOMPHPINIFILE}
+    echo "always_populate_raw_post_data=-1" >> ${CUSTOMPHPINIFILE}
+}
+
+phpinstall() {
+  yum -y install $packages $repoopt
+  phpsededit
+  if [ ! -f /var/opt/remi/php71/log/php-fpm/www-error.log ]; then
+    touch /var/opt/remi/php71/log/php-fpm/www-error.log
+    chmod 0666 /var/opt/remi/php71/log/php-fpm/www-error.log
+    chown nginx:nginx /var/opt/remi/php71/log/php-fpm/www-error.log
+  fi
+  echo "nano -w /etc/opt/remi/php71/php-fpm.d/www.conf" >/usr/bin/fpmconfphp71 ; chmod 700 /usr/bin/fpmconfphp71
+  echo "nano -w /usr/local/nginx/conf/php71-remi.conf" >/usr/bin/phpincphp71 ; chmod 700 /usr/bin/phpincphp71
+  cp -a /usr/local/nginx/conf/php.conf /usr/local/nginx/conf/php71-remi.conf
+  sed -i 's|\[www\]|\[php71-www\]|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|9000|9900|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|9000|9900|' /usr/local/nginx/conf/php71-remi.conf
+  sed -i 's|;listen.backlog = .*|;listen.backlog = 511|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|;listen.owner = .*|listen.owner = nginx|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|;listen.group = .*|listen.group = nginx|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|user = apache|user = nginx|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  sed -i 's|group = apache|group = nginx|' /etc/opt/remi/php71/php-fpm.d/www.conf
+  echo
+  echo "start php71-php-fpm service"
+  systemctl start php71-php-fpm
+  systemctl enable php71-php-fpm
+  echo
+  echo "status php71-php-fpm service"
+  systemctl status php71-php-fpm
+  echo
+  echo "command shortcuts"
+  echo "phpincphp71 - edit /usr/local/nginx/conf/php71-remi.conf include file"
+  echo "fpmconfphp71 - edit /etc/opt/remi/php71/php-fpm.d/www.conf php-fpm config"
+  echo
+  echo "php71 -v"
+  php71 -v
+  echo
+  echo "which php71"
+  which php71
+  echo
+  echo "php71 -m"
+  php71 -m
+  echo "php71 --ini"
+  php71 --ini
+  echo
+}
+
+phpupdate() {
+  yum -y update $packages $repoopt
+}
+
+phplist() {
+  yum list $packages $repoopt
+}
+
+case "$1" in
+  install )
+    phpinstall
+    ;;
+  update )
+    phpupdate
+    ;;
+  list )
+    phplist
+    ;;
+  phpconfig )
+    if [ -f /opt/remi/php71/root/usr/bin/php-config ]; then
+      /opt/remi/php71/root/usr/bin/php-config
+    fi
+    ;;
+  phperrors )
+    if [ -f /var/opt/remi/php71/log/php-fpm/www-error.log ]; then
+      echo "tail -100 /var/opt/remi/php71/log/php-fpm/www-error.log"
+      tail -100 /var/opt/remi/php71/log/php-fpm/www-error.log
+    fi
+    ;;
+  phpcustom )
+    if [ -f /etc/opt/remi/php71/php.d/zzz_customphp.ini ]; then
+      nano /etc/opt/remi/php71/php.d/zzz_customphp.ini
+    fi
+    ;;
+  phpini )
+    php71 --ini
+    ;;
+  phpext )
+    php71 -m
+    ;;
+  start )
+    systemctl start php71-php-fpm
+    ;;
+  restart )
+    systemctl restart php71-php-fpm
+    ;;
+  stop )
+    systemctl stop php71-php-fpm
+    ;;
+  status )
+    systemctl status php71-php-fpm
+    ;;
+  * )
+    echo "$0 {install|update|list|phpconfig|phperrors|phpcustom|phpini|phpext|start|restart|stop|status}"
+    ;;
+esac
